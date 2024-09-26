37d7c1e0530b3a5e1cbc2ec98ca50df5a6a34ffd
Sachintha Waduge <isurusachin123@gmail.com>
2024-09-26 21:37:21 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGODB
+)
+.then(()=> {
+    console.log('MongoDb is connected');
+}).catch((err)=>{
+    console.log(err);
+});
+
+const app = express();
+app.use(express.json());
+
+app.listen(8090,()=> {
+    console.log('Server is running on port 8090!!')
+})
+
+app.use('/api/test',userRoutes);
